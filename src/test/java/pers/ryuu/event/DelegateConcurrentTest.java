1285e0684558a406d747b70fe2e2261b44cb6abc
WIN-KUYOU\DESKTOP <2357622935@qq.com>
2022-07-12 09:42:41 +0800

    multi cast delegate add impl add delegate unit test

--- src/test/java/pers/ryuu/event/DelegateConcurrentTest.java
+++ src/test/java/pers/ryuu/event/DelegateConcurrentTest.java
@@ -16,7 +16,7 @@ public class DelegateConcurrentTest {
         IAction test3 = () -> res.add(3);
         IAction test4 = () -> res.add(4);
         IAction test5 = () -> res.add(5);
-        IAction removeTest3 = () -> action.remove(test3);
+        IAction removeTest3 = () -> System.out.println(action.remove(test3));
 
         action.add(test1);
         action.add(test2);
@@ -27,20 +27,27 @@ public class DelegateConcurrentTest {
 
         action.invoke();
 
-        assert res.contains(1);
-        assert res.contains(2);
-        assert res.contains(3);
-        assert res.contains(4);
-        assert res.contains(5);
+        assert res.equals(new ArrayList<>() {
+            {
+                add(1);
+                add(2);
+                add(3);
+                add(4);
+                add(5);
+            }
+        });
         System.out.println(res);
 
         res.clear();
         action.invoke();
-
-        assert res.contains(1);
-        assert res.contains(2);
-        assert res.contains(4);
-        assert res.contains(5);
+        assert res.equals(new ArrayList<>() {
+            {
+                add(1);
+                add(2);
+                add(4);
+                add(5);
+            }
+        });
         System.out.println(res);
     }
 
@@ -54,7 +61,7 @@ public class DelegateConcurrentTest {
         IAction test3 = () -> res.add(3);
         IAction test4 = () -> res.add(4);
         IAction test5 = () -> res.add(5);
-        IAction removeTest3 = () -> action.remove(test3);
+        IAction removeTest3 = () -> System.out.println(action.remove(test3));
 
         action.add(test1);
         action.add(test2);
@@ -65,10 +72,14 @@ public class DelegateConcurrentTest {
 
         action.invoke();
 
-        assert res.contains(1);
-        assert res.contains(2);
-        assert res.contains(4);
-        assert res.contains(5);
+        assert res.equals(new ArrayList<>() {
+            {
+                add(1);
+                add(2);
+                add(4);
+                add(5);
+            }
+        });
         System.out.println(res);
     }
 
@@ -82,7 +93,7 @@ public class DelegateConcurrentTest {
         IAction test3 = () -> res.add(3);
         IAction test4 = () -> res.add(4);
         IAction test5 = () -> res.add(5);
-        IAction removeTest3 = () -> action.remove(test3);
+        IAction removeTest3 = () -> System.out.println(action.remove(test3));
 
         action.add(test1);
         action.add(test2);
@@ -94,13 +105,120 @@ public class DelegateConcurrentTest {
         action.add(test5);
 
         action.invoke();
+        assert res.equals(new ArrayList<>() {
+            {
+                add(1);
+                add(2);
+                add(3);
+                add(4);
+                add(5);
+            }
+        });
+        System.out.println(res);
+    }
 
-        assert res.contains(1);
-        assert res.contains(2);
-        assert res.contains(3);
-        assert res.contains(4);
-        assert res.contains(5);
+    @Test
+    void removeReturnValue() {
+        Action action = new Action();
+        IAction test1 = () -> System.out.println(1);
+
+        action.add(test1);
+        boolean removeResult1 = action.remove(test1);
+        boolean removeResult2 = action.remove(test1);
+        assert removeResult1;
+        assert !removeResult2;
+    }
+
+    @Test
+    void addMethodMoreMultiTime() {
+        ArrayList<Integer> res = new ArrayList<>();
+        Action action = new Action();
+        IAction test1 = () -> res.add(1);
+
+        action.add(test1);
+        action.add(test1);
+        action.add(test1);
+        action.add(test1);
+        action.add(test1);
+
+        action.invoke();
+        assert res.equals(new ArrayList<>() {
+            {
+                add(1);
+                add(1);
+                add(1);
+                add(1);
+                add(1);
+            }
+        });
+        System.out.println(res);
+    }
+
+    @Test
+    void add() {
+        ArrayList<Integer> res = new ArrayList<>();
+
+        Action action = new Action();
+        IAction test1 = () -> res.add(1);
+        IAction test2 = () -> res.add(2);
+        IAction test3 = () -> res.add(3);
+        IAction test4 = () -> res.add(4);
+        IAction test5 = () -> res.add(5);
+        IAction addTest3 = () -> System.out.println(action.add(test3));
+
+        action.add(test1);
+        action.add(test2);
+        action.add(addTest3);
+        action.add(test3);
+        action.add(test4);
+        action.add(test5);
+
+        action.invoke();
 
         System.out.println(res);
+        assert res.equals(new ArrayList<>() {
+            {
+                add(1);
+                add(2);
+                add(3);
+                add(4);
+                add(5);
+                add(3);
+            }
+        });
+    }
+
+    @Test
+    void addLast() {
+        ArrayList<Integer> res = new ArrayList<>();
+
+        Action action = new Action();
+        IAction test1 = () -> res.add(1);
+        IAction test2 = () -> res.add(2);
+        IAction test3 = () -> res.add(3);
+        IAction test4 = () -> res.add(4);
+        IAction test5 = () -> res.add(5);
+        IAction addTest3 = () -> System.out.println(action.add(test3));
+
+        action.add(test1);
+        action.add(test2);
+        action.add(test3);
+        action.add(test4);
+        action.add(test5);
+        action.add(addTest3);
+
+        action.invoke();
+
+        System.out.println(res);
+        assert res.equals(new ArrayList<>() {
+            {
+                add(1);
+                add(2);
+                add(3);
+                add(4);
+                add(5);
+                add(3);
+            }
+        });
     }
 }
