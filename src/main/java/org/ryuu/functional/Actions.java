5fdfd1689da3abaf0ea6025914683987d298f1ca
Ryuu-64 <2357622935@qq.com>
2025-08-25 00:50:35 +0800

    refactor: replace Actions constructor with factory methods delegate() and event()
    BREAKING CHANGE:
    - Direct constructor usage of Actions is removed.
    - Use Actions.delegate() or Actions.event() factory methods instead.
    
    feat: add Event interface similar to .NET 'event' keyword
    
    BREAKING CHANGE:
    - Introduced Event<T extends Delegate> interface with add/remove methods.
    - MulticastDelegate now implements Event and enforces this contract.
    
    refactor: add thread-safe implementation to MulticastDelegate
    
    BREAKING CHANGE:
    - MulticastDelegate add/remove are now thread-safe, using sync/async depending on event mode.
    - This may alter concurrency behavior compared to previous versions.
    
    chore(release): bump version 3.0.1 → 4.0.0

--- src/main/java/org/ryuu/functional/Actions.java
+++ src/main/java/org/ryuu/functional/Actions.java
@@ -3,6 +3,18 @@ package org.ryuu.functional;
 public final class Actions
         extends MulticastDelegate<Action>
         implements Action {
+    private Actions(boolean isEvent) {
+        super(isEvent);
+    }
+
+    public static Actions delegate() {
+        return new Actions(false);
+    }
+
+    public static Actions event() {
+        return new Actions(true);
+    }
+
     @Override
     public void invoke() {
         for (Action action : this) {
